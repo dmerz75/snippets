47d4c68c9b505f1af8ac40ab3ba6143c33cdc319
Dale R. Merz Jr <merz_drm@hotmail.com>
2014-04-16 01:07:35 -0400

    added c-mode,c++-mode,and python-mode snippets

---
+++ c-mode/MyFunctions.h
@@ -0,0 +1,13 @@
+
+/* ---------------------------------------------------------
+   The header file: MyFunctions.h   
+   --------------------------------------------------------- */
+
+#ifndef MYFUNCTIONS_H
+#define MYFUNCTIONS_H
+// list descriptive part of the function -- usually just the prototype(s)
+
+void myFunc(int &);
+
+// any other functions that you might want to include could go here too.
+#endif
